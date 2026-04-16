2d6f24fd2336213040d96bd187b1f2b849d42190
5Stygian <121655781+5Stygian@users.noreply.github.com>
2026-04-16 13:36:34 -0700

    Update numberDisplay.tsx

--- src/app/components/numberDisplay.tsx
+++ src/app/components/numberDisplay.tsx
@@ -1,6 +1,6 @@
 export default function NumberDisplay() {
   return (
-    <div id="NumberDisplayOuter" className="min-w-400 justify-center items-center p-6 border-2 border-violet-500 rounded-xl">
+    <div id="NumberDisplayOuter" className="max-block-48 justify-center items-center p-6 border-2 border-violet-500 rounded-xl">
       <div id="NumberDisplay">0</div>
       <div id="AttributeDisplay" />
       <div>
