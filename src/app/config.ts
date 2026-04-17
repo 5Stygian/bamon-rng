a03361cd29137ffb56a77cd61560bff9385aa3c4
5Stygian <rlcrafistohard@gmail.com>
2026-04-16 22:18:55 -0700

    make font bold

--- src/app/config.ts
+++ src/app/config.ts
@@ -6,7 +6,7 @@ export const config: Config = {
       "block w-24 m-auto my-8 p-4 font-bold border-2 rounded-2xl border-black \
       bg-gradient-to-br from-fuchsia-500 to-violet-500/80",
     NumberDisplay:
-      "max-w-xl m-auto p-6 border-2 rounded-xl border-violet-500 \
+      "max-w-xl m-auto p-6 font-bold border-2 rounded-xl border-violet-500 \
       bg-gradient-to-tr from-purple-600 to-violet-800",
   },
 };
