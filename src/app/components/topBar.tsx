9864d31b6d0e93fc9f970c07f4667f1cdf9193ab
5Stygian <121655781+5Stygian@users.noreply.github.com>
2026-04-17 11:47:06 -0700

    fuck

--- src/app/components/topBar.tsx
+++ src/app/components/topBar.tsx
@@ -2,8 +2,8 @@ import { config } from "../config";
 
 export default function TopBar() {
   return (
-    <nav className={config.styles.TobBar.Box}>
-      <h1 className={config.styles.TobBar.Title}>Bamon RNG</h1>
+    <nav className={config.styles.TopBar.Box}>
+      <h1 className={config.styles.TopBar.Title}>Bamon RNG</h1>
     </nav>
   );
 }
