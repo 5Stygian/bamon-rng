e684bb9f2078345ae7f3ae0e4692d222bbfee944
5Stygian <rlcrafistohard@gmail.com>
2026-04-17 16:34:55 -0700

    [skip ci]

--- src/app/config.ts
+++ src/app/config.ts
@@ -3,8 +3,8 @@ export const config = {
     RollButton:
       "block w-24 m-auto my-8 p-4 font-bold border-2 rounded-2xl border-black \
       bg-gradient-to-br from-fuchsia-500 to-violet-500/80 \
-      duration-400 hover:cursor-pointer hover:transition-all hover:hue-rotate-40 \
-      hover:scale-120 hover:rotate-4",
+      duration-400 hover:cursor-pointer hover:transition-all hover:hue-rotate-30 \
+      hover:scale-120 hover:rotate-4 hover:brightness-110",
     NumberDisplay:
       "max-w-xl m-auto p-6 font-bold border-2 rounded-xl border-violet-500 \
       bg-gradient-to-tr from-purple-600 to-violet-800 \
